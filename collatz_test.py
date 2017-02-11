2985960af81b4594bd582b6894a0d6b7f2b2bfef
Kyle Gengler <kyle.gengler@gmail.com>
2017-02-11 14:53:51 -0600

    finish up unit tests

--- collatz_test.py
+++ collatz_test.py
@@ -1,25 +1,32 @@
 import unittest
-from collatz import collatz
+import collatz as c
 
 class CollatzTest(unittest.TestCase):
+
+    def test_hotpo_even(self):
+        self.assertEqual(c.hotpo(20), 10)
+
+    def test_hotpo_odd(self):
+        self.assertEqual(c.hotpo(7), 22)
+
     def test_collatz_1(self):
-        self.assertEqual(collatz(1), 0)
+        self.assertEqual(c.collatz(1), 0)
 
     def test_collatz_2(self):
-        self.assertEqual(collatz(2), 1)
+        self.assertEqual(c.collatz(2), 1)
 
     def test_collatz_9(self):
-        self.assertEqual(collatz(9), 19)
+        self.assertEqual(c.collatz(9), 19)
 
     def test_collatz_27(self):
-        self.assertEqual(collatz(27), 111)
+        self.assertEqual(c.collatz(27), 111)
 
     def test_collatz_97(self):
-        self.assertEqual(collatz(97), 118)
+        self.assertEqual(c.collatz(97), 118)
 
     def test_collatz_871(self):
-        self.assertEqual(collatz(871), 178)
+        self.assertEqual(c.collatz(871), 178)
 
     def test_collatz_6171(self):
-        self.assertEqual(collatz(6171), 261)
+        self.assertEqual(c.collatz(6171), 261)
 
