49f6bc3f7cc5191ca19a6af1239ed4a4ff551c8f
Kyle Gengler <kyle.gengler@gmail.com>
2017-02-11 15:07:02 -0600

    add "main" method to match original output

--- collatz.py
+++ collatz.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import argparse
 
 def hotpo(n):
     if n % 2 == 0:
@@ -10,4 +12,16 @@ def collatz(n, acc = 0):
         return acc
     else:
         return collatz(hotpo(n), acc + 1)
+
+if __name__ == '__main__':
+    # create cli arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num", type=int)
+    args = parser.parse_args()
+
+    num = args.num
+    count = collatz(num)
+
+    print("\nCollatz Conjecture for %d" % num)
+    print("Reached 1 in %d steps.\n" % count)
 
